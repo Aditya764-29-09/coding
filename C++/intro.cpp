7aaac6e977a2fb571e414f47695eefaa8e07294f
Aditya Anand <aditya29sep1999@outlook.com>
2022-06-25 13:01:48 +0530

    rename

---
+++ C++/intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //iostream is a header file which is input output stream
+// headerfiles later
+using namespace std; // namespace is used for seperating the variables or same name 
+
+int main()// this is main function starting part of code
+{
+
+//the code comes here
+}
+
+
