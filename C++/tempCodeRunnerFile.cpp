ca2e5acf5cfd3fa56e489f01a54add589960186a
Aditya Anand <aditya29sep1999@outlook.com>
2022-06-25 16:57:52 +0530

    file arrangement

---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//i have to write table of a number
+
+cout<<" table of which number";
+int a;
+cin>>a;
+
+
+
+}
---
+++ C++/whileloop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    // i want to print n stars 
+
+ cout << "how many times you need to print  ";
+
+ int a;
+ cin>> a;
+ int i =0;      //in while loop you have to initialize the counter
+//in while you have to form a condition
+ while(i<=a)
+ {
+
+    cout<<"****"<<endl;
+    i++;
+ }
+}
